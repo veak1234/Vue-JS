a511222833fb09805cff1108ee8a156f8c62bfce
KHLORP.VEAK <KHLORP.VEAK@PNC.LAN>
2024-05-29 20:40:55 +0700

    delete footer

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,9 @@
 <template>
+  <NavbarVue />
   <div class="container">
-    <NavbarVue />
     <div class="mb-5">
       <router-view></router-view>
     </div>
-    <section class="">
-      <!-- Footer -->
-      <footer class="text-center text-white" style="background-color: #0a4275">
-        <!-- Grid container -->
-        <div class="container p-4 pb-0">
-          <!-- Section: CTA -->
-          <section class="">
-            <p class="d-flex justify-content-center align-items-center">
-              <span class="me-3">Register for free</span>
-              <button
-                data-mdb-ripple-init
-                type="button"
-                class="btn btn-outline-light btn-rounded"
-              >
-                Sign up!
-              </button>
-            </p>
-          </section>
-          <!-- Section: CTA -->
-        </div>
-        <!-- Grid container -->
-
-        <!-- Copyright -->
-        <div
-          class="text-center p-3"
-          style="background-color: rgba(0, 0, 0, 0.2)"
-        >
-          © 2020 Copyright:
-          <a class="text-white" href="https://mdbootstrap.com/"
-            >MDBootstrap.com</a
-          >
-        </div>
-        <!-- Copyright -->
-      </footer>
-      <!-- Footer -->
-    </section>
   </div>
 </template>
 
