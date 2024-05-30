77fc9e22ed8d729b69a39a2a37b1235561609313
KHLORP.VEAK <KHLORP.VEAK@PNC.LAN>
2024-05-30 15:11:16 +0700

    create life vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Navbar</a>
       <button
